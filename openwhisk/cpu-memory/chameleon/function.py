0d4b173e1207f8864a998fdae6b6ee8950c0a9d5
xkf15 <13051775153@163.com>
2023-11-13 21:47:04 -0500

    Add marker for begin and end tracing

--- openwhisk/cpu-memory/chameleon/function.py
+++ openwhisk/cpu-memory/chameleon/function.py
@@ -16,8 +16,32 @@ tal:content="python: d" />
 </tr>
 </table>""" % six.text_type.__name__
 
+buf_s = mmap.mmap(-1, mmap.PAGESIZE, prot=mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC)
+ftype = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int)
+fpointer_s = ctypes.c_void_p.from_buffer(buf_s)
+f_s = ftype(ctypes.addressof(fpointer_s))
+
+buf_s.write(
+    b'\x8b\xc7'  # mov eax, edi
+    b'\x83\xc0\x01'  # add eax, 1
+    b'\x0f\x1f\x84\xbe\x00\x00\x01\x01' # nop
+    b'\xc3' #ret
+)
+
+buf_e = mmap.mmap(-1, mmap.PAGESIZE, prot=mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC)
+ftype = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int)
+fpointer_e = ctypes.c_void_p.from_buffer(buf_e)
+f_e = ftype(ctypes.addressof(fpointer_e))
+
+buf_e.write(
+    b'\x8b\xc7'  # mov eax, edi
+    b'\x83\xc0\x01'  # add eax, 1
+    b'\x0f\x1f\x84\xed\x00\x00\x01\x01' # nop
+    b'\xc3' #ret
+)
 
 def main(event):
+    f_s(0)
     latencies = {}
     timestamps = {}
     timestamps["starting_time"] = time()
@@ -39,5 +63,6 @@ def main(event):
     latency = time() - start
     latencies["function_execution"] = latency
     timestamps["finishing_time"] = time()
-
+    
+    f_e(0)
     return {"latencies": latencies, "timestamps": timestamps, "metadata": metadata}
